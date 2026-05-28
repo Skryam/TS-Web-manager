4fad61f41803f26d75a0c116eed64d71d352063a
Skryam <skryams@gmail.com>
2026-05-28 17:43:48 +0500

    up

---
+++ frontend/src/components/UsersList.tsx
@@ -0,0 +1,46 @@
+import { useQuery } from '@apollo/client/react';
+import { GET_USERS } from '../graphql/queries';
+import { Table, Spinner, Alert, Container } from 'react-bootstrap';
+
+interface GetUsersData {
+  users: {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+  }[];
+}
+
+export default function UsersList() {
+  const { loading, error, data } = useQuery<GetUsersData>(GET_USERS);
+
+  if (loading) return <Spinner animation="border" role="status" />;
+  if (error) return <Alert variant="danger">Ошибка: {error.message}</Alert>;
+  if (!data) return null;
+
+  return (
+    <Container className="mt-4">
+      <h2>Пользователи</h2>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Имя</th>
+            <th>Фамилия</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
+}
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Welcome from './welcome.tsx'
+import { ApolloProvider } from '@apollo/client/react';
+import client from './apollo/client.ts';
+import Welcome from './components/welcome.tsx';
 
-const App = (props) => {
+const App = () => {
   return (
     <body className='d-flex flex-column min-vh-100 bg-light'>
       <nav className='navbar navbar-expand-lg navbar-light mb-3 bg-secondary bg-opacity-25 container-fluid'>
         <a className='navbar-brand' href='/'>Рут</a>
         <button className='navbar-toggler' data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent">
           <span className='navbar-toggler-icon' />
+          <ul className='navbar-nav.mr-auto'>
+            <li className='nav-item'>
+              <a className='nav-link' href='/'>Юзеры</a>
+            </li>
+          </ul>
         </button>
       </nav>
       <Welcome />
@@ -18,6 +25,10 @@ const App = (props) => {
 }
 
 
-const mountNode = document.getElementById('root');
+const mountNode = document.getElementById('root')!;
 const root = ReactDOM.createRoot(mountNode);
-root.render(<App />)
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
